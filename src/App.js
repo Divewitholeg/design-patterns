b26ddc30bd3b72160938615b15e4dfc465eb2269
vuewebdev <rybniko@gmail.com>
2023-04-30 15:50:23 +0300

    Split layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandSide = ({ name }) => {
+  return <div style={{ backgroundColor: "fuchsia" }}>{name}</div>;
+};
+
+const RightHandSide = ({ message }) => {
+  return <div style={{ backgroundColor: "teal" }}>{message}</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandSide name={"Oleg"} />
+      <RightHandSide message={"Hi from the right!"} />
+    </SplitScreen>
   );
 }
 
